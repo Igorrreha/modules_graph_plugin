647e051b9c91ee70c9e5acf2969fb2b01a9236c1
Igorrreha <igorecha.9999@gmail.com>
2023-08-14 22:01:50 +0700

    Add method to provide many graph nodes

--- addons/modules_graph/modules/core_visualization_graph/graph_nodes_provider/graph_nodes_provider.gd
+++ addons/modules_graph/modules/core_visualization_graph/graph_nodes_provider/graph_nodes_provider.gd
@@ -12,6 +12,16 @@ func provide(content: Entity) -> MgpGraphNode:
 	return _nodes_by_content.get(content)
 
 
+func provide_many(content_array: Array[Entity]) -> Array[MgpGraphNode]:
+	var nodes: Array[MgpGraphNode]
+	
+	for content in content_array:
+		if _nodes_by_content.has(content):
+			nodes.append(_nodes_by_content[content])
+	
+	return nodes
+
+
 func _ready() -> void:
 	_graph.child_entered_tree.connect(_on_graph_child_entered_tree)
 	_graph.child_exiting_tree.connect(_on_graph_child_exiting_tree)
